42dbeab095a38d21525326e641adf6f2f6da3c2d
barwis00 <wisnuakbar18@gmail.com>
2019-11-25 01:18:59 +0700

    Update pages/public/kurikulum

--- pages/public/kurikulum.php
+++ pages/public/kurikulum.php
@@ -1,5 +1,86 @@
 <?php include '../static/top.php'; ?>
   <section class="content">
-    ISINYA DISINI YA
+  	<div class="col-xs-6">
+      <div class="box">
+
+            <table class="table table-bordered text-center">
+                <thead>
+                	<tr>
+                		<th colspan="6" style="background-color: aqua;">Semester 1</th>
+                	</tr>
+                    <tr>
+                        <th rowspan="2" style="vertical-align: middle;">Kode</th>
+                        <th rowspan="2" style="vertical-align: middle;">Mata Kuliah</th>
+                        <th colspan="2" >SKS</th>
+                        <th rowspan="2" style="vertical-align: middle;">Jenis</th>
+                    </tr>
+                    <tr>
+                        <th>Teori</th>
+                        <th>Lab</th>
+                    </tr>
+                </thead>
+                <tbody>
+                     <tr>
+                        <td class="text-left">CSSE303</td>
+                        <td>Rekayasa Perangkat Lunak</td>
+                        <td>3</td>
+                        <td>0</td>
+                        <td>Wajib</td>
+                        </tr>  
+                       <tr>
+                        <td class="text-left">KKES201</td>
+                        <td>Analisis dan Perancangan Berorientasi Objek</td>
+                        <td>2</td>
+                        <td>1</td>
+                        <td>Wajib</td>
+                        </tr>  
+
+                </tbody>
+                 </table>
+
+    </div><!-- /.box -->
+</div>
+
+<div class="col-xs-6">
+      <div class="box">
+       
+        
+            <table class="table table-bordered text-center">
+                <thead>
+                	<tr>
+                		<th colspan="6" style="background-color: aqua;">Semester 1</th>
+                	</tr>
+                    <tr>
+                        <th rowspan="2" style="vertical-align: middle;">Kode</th>
+                        <th rowspan="2" style="vertical-align: middle;">Mata Kuliah</th>
+                        <th colspan="2" >SKS</th>
+                        <th rowspan="2" style="vertical-align: middle;">Jenis</th>
+                    </tr>
+                    <tr>
+                        <th>Teori</th>
+                        <th>Lab</th>
+                    </tr>
+                </thead>
+                <tbody>
+                     <tr>
+                        <td class="text-left">CSSE303</td>
+                        <td>Rekayasa Perangkat Lunak</td>
+                        <td>3</td>
+                        <td>0</td>
+                        <td>Wajib</td>
+                        </tr>  
+                       <tr>
+                        <td class="text-left">KKES201</td>
+                        <td>Analisis dan Perancangan Berorientasi Objek</td>
+                        <td>2</td>
+                        <td>1</td>
+                        <td>Wajib</td>
+                        </tr>  
+
+                </tbody>
+                 </table>
+    
+    </div><!-- /.box -->
+</div>
   </section>
 <?php include '../static/bot.php'; ?>
