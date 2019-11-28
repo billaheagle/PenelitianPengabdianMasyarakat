ee3c9bac865bff5d6ab5d319962259c3bb697e4b
barwis00 <wisnuakbar18@gmail.com>
2019-11-28 16:12:49 +0700

    Update autocomplete dan input name penelitian & pengabdian masyarakat

--- pages/kelola-data/penelitian.php
+++ pages/kelola-data/penelitian.php
@@ -97,25 +97,25 @@
                         <div class="col-xs-6">
                             <div class="form-group">
                             <label>Tema</label>
-                            <input type="text" placeholder="Tema" name="kodematakuliah" class="form-control">
+                            <input type="text" placeholder="Tema" name="tema" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-6">
                              <div class="form-group">
                             <label>Judul</label>
-                            <input type="text" placeholder="Judul" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Judul" name="judul" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-12">
                              <div class="form-group">
                             <label>Deskripsi</label>
-                            <textarea class="form-control" rows="3"></textarea>
+                            <textarea class="form-control" rows="3" name="deskripsi"></textarea>
                             </div>
                         </div>
                         <div class="col-xs-6">
                              <div class="form-group">
                             <label>Tempat</label>
-                            <input type="text" placeholder="Tempat" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Tempat" name="tempat" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-6">
@@ -125,14 +125,14 @@
                                 <div class="input-group-addon">
                                 <i class="fa fa-calendar"></i>
                                 </div>
-                                <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask="">
+                                <input type="text" class="form-control" name="tanggal" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask="" autocomplete="off">
                                 </div>
                             </div>
                         </div>
                         <div class="col-xs-6">
                             <div class="form-group">
                             <label>Sumber Pembiayaan</label>
-                            <input type="text" placeholder="Sumber Pembiayaan" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Sumber Pembiayaan" name="sumber_pembiayaan" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-6">
@@ -140,21 +140,21 @@
                             <label>Biaya</label>
                             <div class="input-group">
                             <span class="input-group-addon" style="background-color: rgb(230, 230, 230);">Rp.</span>
-                            <input type="text" placeholder="Biaya" class="form-control">
+                            <input type="text" name="biaya" placeholder="Biaya" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         </div>
                         <div class="col-xs-12">
                             <div class="form-group">
                             <label>Evaluasi</label>
-                            <textarea class="form-control" rows="3"></textarea>
+                            <textarea class="form-control" name="evaluasi" rows="3"></textarea>
                             </div>
                         </div>
                          <div class="col-xs-12">
                             <div class="form-group">
-                            <label for=" Mahasiswa">Mahasiswa yang terlibat</label>
+                            <label for=" Mahasiswa">Mahasiswa yang Terlibat</label>
                                 <div class="input-group">
-                                    <input type="text" placeholder="Mahasiswa" class="form-control">
+                                    <input type="text" name="id_user" placeholder="Mahasiswa" class="form-control" autocomplete="off">
                                 <span class="input-group-btn">
                                 <button class="btn btn-flat btn-secondary" type="file">Tambah</button>
                                 </span>
@@ -164,13 +164,13 @@
                         <div class="col-xs-6">
                             <div class="form-group">
                             <label>Integrasi</label>
-                            <input type="text" placeholder="Integrasi" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Kode Mata Kuliah" name="id_matakuliah" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-6">
                             <div class="custom-file">
                                 <label>File</label>
-                                <input type="file" class="custom-file-input" id="customFile">
+                                <input type="file" class="custom-file-input" name="file" id="customFile">
                             </div>   
                         </div>    
                     
@@ -192,28 +192,29 @@
                 </div>
                 <form action="#">
                 <div class="modal-body row">
+                    <input name="id" type="hidden" value="">
                         <div class="col-xs-6" >
                             <div class="form-group">
                             <label>Tema</label>
-                            <input type="text" placeholder="Tema" name="kodematakuliah" class="form-control">
+                            <input type="text" placeholder="Tema" name="tema" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-6">
                             <div class="form-group">
                             <label>Judul</label>
-                            <input type="text" placeholder="Judul" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Judul" name="judul" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-12">
                             <div class="form-group">
                             <label>Deskripsi</label>
-                            <textarea class="form-control" rows="3"></textarea>
+                            <textarea class="form-control" name="deskripsi" rows="3"></textarea>
                             </div>
                         </div>
                         <div class="col-xs-6">
                             <div class="form-group">
                             <label>Tempat</label>
-                            <input type="text" placeholder="Tempat" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Tempat" name="tempat" class="form-control" autocomplete="off">
                             </div>
                         </div>
                        <div class="col-xs-6">
@@ -223,14 +224,14 @@
                                 <div class="input-group-addon">
                                 <i class="fa fa-calendar"></i>
                                 </div>
-                                <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask="">
+                                <input type="text" name="tanggal" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask="" autocomplete="off">
                                 </div>
                             </div>
                         </div>
                         <div class="col-xs-6">
                             <div class="form-group">
                             <label>Sumber Pembiayaan</label>
-                            <input type="text" placeholder="Sumber Pembiayaan" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Sumber Pembiayaan" name="sumber_pembiayaan" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-6">
@@ -238,21 +239,21 @@
                             <label>Biaya</label></br>
                                 <div class="input-group">
                                 <span class="input-group-addon" style="background-color: rgb(230, 230, 230);">Rp.</span>
-                                <input type="text" placeholder="Biaya" class="form-control">
+                                <input type="text" name="biaya" placeholder="Biaya" class="form-control" autocomplete="off">
                                 </div>
                             </div>
                         </div>
                         <div class="col-xs-12">
                             <div class="form-group">
                             <label>Evaluasi</label></br>
-                            <textarea class="form-control" rows="3"></textarea>
+                            <textarea class="form-control" name="biaya" rows="3"></textarea>
                             </div>
                         </div>
                          <div class="col-xs-12">
                             <div class="form-group">
-                            <label for=" Mahasiswa">Mahasiswa yang terlibat</label>
+                            <label for=" Mahasiswa">Mahasiswa yang Terlibat</label>
                                 <div class="input-group">
-                                <input type="text" placeholder="Mahasiswa" class="form-control">
+                                <input type="text" placeholder="Mahasiswa" name="id_user" class="form-control" autocomplete="off">
                                 <span class="input-group-btn">
                                 <button class="btn btn-flat btn-secondary" type="file">Tambah</button>
                                 </span>
@@ -262,13 +263,13 @@
                         <div class="col-xs-6">
                             <div class="form-group">
                             <label>Integrasi</label></br>
-                            <input type="text" placeholder="Integrasi" name="matakuliah" class="form-control">
+                            <input type="text" placeholder="Kode Mata Kuliah" name="id_matakuliah" class="form-control" autocomplete="off">
                             </div>
                         </div>
                         <div class="col-xs-6">
                              <div class="custom-file">
                                 <label>File</label>
-                                <input type="file" class="custom-file-input" id="customFile">
+                                <input type="file" name="file" class="custom-file-input" id="customFile">
                             </div>   
                         </div>    
                     </form>
@@ -287,13 +288,16 @@
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title">Delete Data</h4>
                 </div>
-                <div class="modal-body">
-                    <p>Apakah anda yakin ingin menghapus data ini?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-danger">Delete</button>
-                </div>
+                <form action="#">
+                    <input name="id" type="hidden" value="">
+                    <div class="modal-body">
+                        <p>Apakah anda yakin menghapus data ini?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </div>
+                </form>
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.example-modal -->
